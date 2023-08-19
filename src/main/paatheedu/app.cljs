c9fbdffcee9dea04bb5c57730925636278c91c7f
Tamizhvendan S <tamizh88@gmail.com>
2023-08-19 10:28:43 +0530

    add table rendering based on file upload content

--- src/main/paatheedu/app.cljs
+++ src/main/paatheedu/app.cljs
@@ -1,12 +1,50 @@
 (ns paatheedu.app
-  (:require [uix.core :refer [defui $]]
-            [uix.dom]))
+  (:require [uix.core :as uix :refer [defui $]]
+            [uix.dom]
+            [goog.labs.format.csv :as csv]
+            [clojure.string :as str]))
 
 (defonce root
   (uix.dom/create-root (js/document.getElementById "root")))
 
+(defn- format-csv [parsed-csv]
+  (->> (filter seq parsed-csv)
+       (map #(map str/trim %))))
+
+#_ (.parse js/Date (clojure.string/trim (first (get d 2))))
+
+(defn- on-file-upload [set-value! ^js e]
+  (.preventDefault e)
+  (let [js-file-reader (js/FileReader.)
+        uploaded-file (-> e .-target .-files first)]
+    (set! (.-onload js-file-reader)
+          (fn [evt]
+            (let [file-content (-> evt .-target .-result)
+                  parsed-content (csv/parse file-content)]
+              (prn file-content)
+              (set-value! parsed-content))))
+    (if uploaded-file
+     (.readAsText js-file-reader uploaded-file) 
+     (set-value! []))))
+
+(defn- render-csv [formatted-csv-content]
+  (let [[header & rows] formatted-csv-content]
+    ($ :table 
+       ($ :thead
+          ($ :tr
+             (map #($ :th {:key (gensym)} %) header)))
+       ($ :tbody
+          (map (fn [row]
+                 ($ :tr {:key (gensym)}
+                    (map #($ :td {:key (gensym)} %) row))) rows)))))
+
 (defui app []
-  ($ :span "Hello, World!"))
+  (let [[value set-value!] (uix/use-state [])]
+    ($ :<> 
+       ($ :input {:type "file" :on-change (partial on-file-upload set-value!)})
+       ($ :div {:class "mt-4"} (-> value
+                                 format-csv
+                                 render-csv)))))
 
 (defn init []
   (uix.dom/render-root ($ app) root))
