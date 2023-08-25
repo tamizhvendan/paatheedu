e48246b0beeb65bc3c5c6f0c551cda99f63c997e
Tamizhvendan S <tamizh88@gmail.com>
2023-08-25 18:46:36 +0530

    add bank statement parser

--- src/main/paatheedu/app.cljs
+++ src/main/paatheedu/app.cljs
@@ -1,50 +1,107 @@
 (ns paatheedu.app
   (:require [uix.core :as uix :refer [defui $]]
             [uix.dom]
-            [goog.labs.format.csv :as csv]
-            [clojure.string :as str]))
+            [goog.object]
+            ["xlsx" :as XLSX]))
 
 (defonce root
   (uix.dom/create-root (js/document.getElementById "root")))
 
-(defn- format-csv [parsed-csv]
-  (->> (filter seq parsed-csv)
-       (map #(map str/trim %))))
+(def hdfc-bank-statement-sheet-schema
+  #:sheet.schema{:transaction-date-column-name "A"
+                 :narration-column-name "B"
+                 :reference-number-column-name "C"
+                 :value-date-column-name "D"
+                 :debit-column-name "E"
+                 :credit-column-name "F"
+                 :closing-balance-column-name "G"
+                 :first-transaction-row-number 23
+                 :sheet-name "Sheet 1"})
 
-#_ (.parse js/Date (clojure.string/trim (first (get d 2))))
+(def idfc-bank-statement-sheet-schema
+  #:sheet.schema{:transaction-date-column-name "A"
+                 :value-date-column-name "B"
+                 :narration-column-name "C"
+                 :reference-number-column-name "D"
+                 :debit-column-name "E"
+                 :credit-column-name "F"
+                 :closing-balance-column-name "G"
+                 :first-transaction-row-number 22
+                 :sheet-name "Account Statement"})
 
-(defn- on-file-upload [set-value! ^js e]
+(defn- extract-transaction-data-from-sheet [{:sheet.schema/keys [transaction-date-column-name
+                                                                 narration-column-name
+                                                                 reference-number-column-name
+                                                                 value-date-column-name
+                                                                 debit-column-name
+                                                                 credit-column-name
+                                                                 closing-balance-column-name
+                                                                 first-transaction-row-number]} sheet]
+  (reduce (fn [acc row-number]
+            (let [transaction-date (get sheet (str transaction-date-column-name row-number))
+                  value-date (get sheet (str value-date-column-name row-number))
+                  narration (get sheet (str narration-column-name row-number))
+                  reference-number (get sheet (str reference-number-column-name row-number))
+                  debit (get sheet (str debit-column-name row-number))
+                  credit (get sheet (str credit-column-name row-number))
+                  closing-balance (get sheet (str closing-balance-column-name row-number))]
+              (if (every? nil? [transaction-date value-date narration
+                                reference-number debit credit closing-balance])
+                (reduced acc)
+                (conj acc #:transaction{:date (get transaction-date "v")
+                                        :value-date (get value-date "v")
+                                        :narration (get narration "v")
+                                        :reference-number (get reference-number "v")
+                                        :debit (get debit "v")
+                                        :credit (get credit "v")
+                                        :closing-balance (get closing-balance "v")}))))
+          []
+          (filter #(<= first-transaction-row-number %) (range))))
+
+(defn- on-file-upload [bank-statement-sheet-schema set-value! ^js e]
   (.preventDefault e)
   (let [js-file-reader (js/FileReader.)
         uploaded-file (-> e .-target .-files first)]
     (set! (.-onload js-file-reader)
           (fn [evt]
             (let [file-content (-> evt .-target .-result)
-                  parsed-content (csv/parse file-content)]
-              (prn file-content)
-              (set-value! parsed-content))))
+                  xl-content (XLSX/read file-content)
+                  extracted-content (extract-transaction-data-from-sheet
+                                     bank-statement-sheet-schema
+                                     (js->clj (unchecked-get (.-Sheets xl-content) 
+                                                     (:sheet.schema/sheet-name bank-statement-sheet-schema))))]
+              (set-value! extracted-content))))
     (if uploaded-file
-     (.readAsText js-file-reader uploaded-file) 
-     (set-value! []))))
+      (.readAsArrayBuffer js-file-reader uploaded-file)
+      (set-value! []))))
 
-(defn- render-csv [formatted-csv-content]
-  (let [[header & rows] formatted-csv-content]
-    ($ :table 
-       ($ :thead
-          ($ :tr
-             (map #($ :th {:key (gensym)} %) header)))
-       ($ :tbody
-          (map (fn [row]
-                 ($ :tr {:key (gensym)}
-                    (map #($ :td {:key (gensym)} %) row))) rows)))))
+
+
+
+(defn- render-transactions [transactions]
+  (when (seq transactions)
+    ($ :table
+      ($ :thead
+         ($ :tr
+            ($ :th "Transaction Date")
+            ($ :th "Narration")
+            ($ :th "Debit")
+            ($ :th "Credit")
+            ($ :th "Closing Balance")))
+      ($ :tbody
+         (map (fn [{:transaction/keys [date narration debit credit closing-balance]}]
+                ($ :tr {:key (gensym)}
+                   ($ :td date)
+                   ($ :td narration)
+                   ($ :td debit)
+                   ($ :td credit)
+                   ($ :td closing-balance))) transactions)))))
 
 (defui app []
   (let [[value set-value!] (uix/use-state [])]
-    ($ :<> 
-       ($ :input {:type "file" :on-change (partial on-file-upload set-value!)})
-       ($ :div {:class "mt-4"} (-> value
-                                 format-csv
-                                 render-csv)))))
+    ($ :<>
+       ($ :input {:type "file" :on-change (partial on-file-upload idfc-bank-statement-sheet-schema set-value!)})
+       ($ :div {:class "mt-4"} (render-transactions value)))))
 
 (defn init []
   (uix.dom/render-root ($ app) root))
